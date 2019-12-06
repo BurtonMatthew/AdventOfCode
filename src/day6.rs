60d8ae585b1637f17182abbff17de575c0df5500
Matt Burton <burtonmatthewa@gmail.com>
2019-12-06 04:13:44 -0500

    Cut down on Day6 allocs

--- src/day6.rs
+++ src/day6.rs
@@ -7,9 +7,9 @@ pub fn part1()
     let mut file = File::open("input/day6.txt").expect("Couldn't find day6 input");
     let mut file_data = String::new();
     file.read_to_string(&mut file_data).expect("Unable to read file");
-    let map = orbiters_map(file_data);
+    let map = orbiters_map(&file_data);
 
-    println!("Day 6 Part 1: {}", orbit_checksum(&map, &String::from("COM"), 0));
+    println!("Day 6 Part 1: {}", orbit_checksum(&map, "COM", 0));
 
 }
 
@@ -18,18 +18,18 @@ pub fn part2()
     let mut file = File::open("input/day6.txt").expect("Couldn't find day6 input");
     let mut file_data = String::new();
     file.read_to_string(&mut file_data).expect("Unable to read file");
-    let map = orbit_map(file_data);
+    let map = orbit_map(&file_data);
 
     println!("Day 6 Part 2: {}", min_transfers(&map));
 }
 
-fn orbiters_map(data: String) -> HashMap<String, Vec<String>>
+fn orbiters_map(data: &String) -> HashMap<&str, Vec<&str>>
 {
-    let mut map : HashMap<String, Vec<String>> = HashMap::new();
+    let mut map : HashMap<&str, Vec<&str>> = HashMap::new();
     for line in data.lines()
     {
-        let inner = line[..3].to_string();
-        let outer = line[4..].to_string();
+        let inner = &line[..3];
+        let outer = &line[4..];
         if let Some(orbiters) = map.get_mut(&inner)
         {
             orbiters.push(outer);
@@ -42,7 +42,7 @@ fn orbiters_map(data: String) -> HashMap<String, Vec<String>>
     map
 }
 
-fn orbit_checksum(map: &HashMap<String, Vec<String>>, root: &String, depth: u32) -> u32
+fn orbit_checksum(map: &HashMap<&str, Vec<&str>>, root: &str, depth: u32) -> u32
 {
     if let Some(orbiters) = map.get(root)
     {
@@ -54,33 +54,31 @@ fn orbit_checksum(map: &HashMap<String, Vec<String>>, root: &String, depth: u32)
     }
 }
 
-fn orbit_map(data: String) -> HashMap<String, String>
+fn orbit_map(data: &String) -> HashMap<&str, &str>
 {
-    let mut map : HashMap<String, String> = HashMap::new();
+    let mut map : HashMap<&str, &str> = HashMap::new();
     for line in data.lines()
     {
-        let inner = line[..3].to_string();
-        let outer = line[4..].to_string();
-        map.insert(outer, inner);
+        map.insert(&line[4..], &line[..3]);
     }
     map
 }
 
-fn min_transfers(map: &HashMap<String, String>) -> usize
+fn min_transfers(map: &HashMap<&str, &str>) -> usize
 {
-    fn get_path<'a>(map: &'a HashMap<String, String>, leaf: &String) -> Vec<&'a String>
+    fn get_path<'a>(map: &'a HashMap<&str, &str>, leaf: &str) -> Vec<&'a str>
     {
         let mut path = Vec::new();
         let mut pos = map.get(leaf).unwrap();
-        while *pos != String::from("COM")
+        while *pos != "COM"
         {
-            path.push(pos);
+            path.push(*pos);
             pos = map.get(pos).unwrap();
         }
         path
     }
-    let you_path = get_path(map, &String::from("YOU"));
-    let san_path = get_path(map, &String::from("SAN"));
+    let you_path = get_path(map, "YOU");
+    let san_path = get_path(map, "SAN");
 
     let mut transfers = 0;
     for (you_index, orbit) in you_path.iter().enumerate()
