cbdeac0c428237cc79d4bb60e285e17164e7a749
Matt Burton <burtonmatthewa@gmail.com>
2020-12-06 17:33:48 -0500

    Bitwise solutions

--- 2020/src/day6.rs
+++ 2020/src/day6.rs
@@ -16,6 +16,17 @@ pub fn part1(input : &Vec<String>) -> usize
     }).sum()
 }
 
+#[aoc(day6, part1, part1_bits)]
+pub fn part1_bits(input : &Vec<String>) -> u32
+{
+    input.iter().map(|s| 
+    {
+        s.bytes().filter(|&c| c != b'\n')
+            .fold(std::u32::MIN, |acc, i| acc | 1 << (i - b'a'))
+            .count_ones()
+    }).sum()
+}
+
 #[aoc(day6, part2)]
 pub fn part2(input : &Vec<String>) -> usize
 {
@@ -33,9 +44,21 @@ pub fn part2(input : &Vec<String>) -> usize
     }).sum()
 }
 
+#[aoc(day6, part2, part2_bits)]
+pub fn part2_bits(input : &Vec<String>) -> u32
+{
+    input.iter().map(|s| 
+    {
+        s.lines()
+            .map(|line| line.bytes().fold(u32::MIN, |acc, i| acc | 1_u32 << (i - b'a')))
+            .fold(std::u32::MAX, |acc, i| acc & i)
+            .count_ones()
+    }).sum()
+}
+
 pub fn intersect(a:&String, b:&String) -> String
 {
-    a.chars().filter(|c| b.contains(*c)).collect()
+    a.chars().filter(|&c| b.contains(c)).collect()
 }
 
 #[aoc(day6, part2, part2_hashset)]
