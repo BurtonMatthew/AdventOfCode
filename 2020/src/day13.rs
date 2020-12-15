0a13f39cfd62121fe00a10889f056d7db57ccc13
Matt Burton <burtonmatthewa@gmail.com>
2020-12-14 23:20:40 -0500

    Sort busses in part 2

--- 2020/src/day13.rs
+++ 2020/src/day13.rs
@@ -1,3 +1,5 @@
+use itertools::Itertools;
+
 pub struct Input
 {
     depart: usize,
@@ -28,102 +30,27 @@ pub fn part1(input : &InputType) -> usize
 }
 
 #[aoc(day13, part2)]
-pub fn part2(input : &InputType) -> i64
-{
-    let mut residues: Vec<i64> = Vec::new();
-    let mut modulii: Vec<i64> = Vec::new();
-
-    for (i, bus) in input.busses.iter().enumerate()
-    {
-        if let Some(x) = bus
-        {
-            modulii.push(*x as i64);
-            residues.push(*x as i64 - i as i64);
-        }
-    }
-
-    chinese_remainder(&residues, &modulii).unwrap()
-}
-
-#[aoc(day13, part2, part2_lcm)]
-pub fn part2_lcm(input : &InputType) -> usize
+pub fn part2(input : &InputType) -> isize
 {
     let mut t = 0;
-    let mut period = input.busses[0].unwrap(); // todo: handle first bus being bad?
-    let mut i = 1;
-    while i < input.busses.len()
+    let mut period = 0;
+    let mut indexed_busses_sorted = input.busses.iter().enumerate().filter_map(|(i, &b)| Some((i,b?))).sorted_by(|(_,b1), (_,b2)| b2.cmp(b1));
+    if let Some((i,b)) =  indexed_busses_sorted.next()
     {
-        // Skip past non-busses
-        if input.busses[i].is_none() 
-        { 
-            i += 1; 
-            continue;
-        }
-        let bus = input.busses[i].unwrap();
-        if (t+i) % bus == 0
+        t = t-i as isize;
+        period = b;
+    }
+
+    for (i,b) in indexed_busses_sorted
+    {
+        while (t+i as isize) % b as isize != 0
         {
-            period = lcm(period,bus);
-            i += 1;
+            t = t + period as isize;
         }
-        else
-        {
-            t = t + period;
-        }
+        period = period *b; // Should LCM but all busses prime
     }
 
     t
-}
-
-// Rosetta code
-use std::cmp::{max, min};
- 
-fn gcd(a: usize, b: usize) -> usize {
-    match ((a, b), (a & 1, b & 1)) {
-        ((x, y), _) if x == y => y,
-        ((0, x), _) | ((x, 0), _) => x,
-        ((x, y), (0, 1)) | ((y, x), (1, 0)) => gcd(x >> 1, y),
-        ((x, y), (0, 0)) => gcd(x >> 1, y >> 1) << 1,
-        ((x, y), (1, 1)) => {
-            let (x, y) = (min(x, y), max(x, y));
-            gcd((y - x) >> 1, x)
-        }
-        _ => unreachable!(),
-    }
-}
- 
-fn lcm(a: usize, b: usize) -> usize {
-    a * b / gcd(a, b)
-}
-
-fn egcd(a: i64, b: i64) -> (i64, i64, i64) {
-    if a == 0 {
-        (b, 0, 1)
-    } else {
-        let (g, x, y) = egcd(b % a, a);
-        (g, y - (b / a) * x, x)
-    }
-}
- 
-fn mod_inv(x: i64, n: i64) -> Option<i64> {
-    let (g, x, _) = egcd(x, n);
-    if g == 1 {
-        Some((x % n + n) % n)
-    } else {
-        None
-    }
-}
- 
-fn chinese_remainder(residues: &[i64], modulii: &[i64]) -> Option<i64> {
-    let prod = modulii.iter().product::<i64>();
- 
-    let mut sum = 0;
- 
-    for (&residue, &modulus) in residues.iter().zip(modulii) {
-        let p = prod / modulus;
-        sum += residue * mod_inv(p, modulus)? * p
-    }
- 
-    Some(sum % prod)
 }
 
 #[cfg(test)]
